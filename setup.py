00fec0fff2da4d7674f1d1f5cc7ae71041b6a1ad
Korbinian Schweiger <korbinian.schweiger@cern.ch>
2020-05-01 17:56:27 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='html_table_parser',
+    license="AGPLv3",
+    version="0.1.1",
+    description='HTMLParser for parsing tbales',
+    author='Josua Schmid',
+    packages=['html_table_parser'],
+)
